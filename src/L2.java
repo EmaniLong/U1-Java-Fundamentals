b87292e952c8a4cddc06fcdf2b014b8d8e6ccd43
longema000 <longema000@warren.k12.in.us>
2021-08-04 11:51:34 -0400

    Completed L1 and L2 Notes

--- src/L2.java
+++ src/L2.java
@@ -1,4 +1,13 @@
 public class L2 {
 
+    public static void main(String[] args){
+       String firstName = "Billy";
+        int age = 26;
+        double balance = 1387.97
+                int accountID = 1829274
+                        System.out.println(balance)
+        System.out.println(firstName);
+        System.out.println(age);
+    }
     //add the main method header
 }
