59cd1f2521af824f3e9c1ef0bd61924da2f8a11b
longema000 <longema000@warren.k12.in.us>
2021-08-12 12:11:39 -0400

    notes

--- 1.5 Practice Problems/src/Average2.java
+++ 1.5 Practice Problems/src/Average2.java
@@ -2,4 +2,9 @@ public class Average2 {
     /*
     Given two numbers, num1 and num2, output the average.
     */
+    public static void main(String[] args) {
+
+        int num1 = 8;
+        int num2 21;
+    }
 }
