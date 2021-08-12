f26dffc7c029814b97ac754e0631f0294ca8b6ad
longema000 <longema000@warren.k12.in.us>
2021-08-12 12:09:35 -0400

    Completed L1 and L2 Notes

--- src/L2.java
+++ src/L2.java
@@ -3,9 +3,9 @@ public class L2 {
     public static void main(String[] args){
        String firstName = "Billy";
         int age = 26;
-        double balance = 1387.97
-                int accountID = 1829274
-                        System.out.println(balance)
+        double balance = 1387.97;
+                int accountID = 1829274;
+        System.out.println(balance);
         System.out.println(firstName);
         System.out.println(age);
     }
--- src/L5.java
+++ src/L5.java
@@ -1,2 +1,13 @@
-public class L5 {
+public class L5{
+    public static void main(String[] args){
+
+        double basePay = 7.25;
+        double hoursWeek1 = 16.5;
+        double hoursWeek2 = 23.75;
+
+        double totalHours = hoursWeek1 + hoursWeek2;
+        double wages = basePay * totalHours;
+
+        System.out.println("Your bi-weekly paycheck is: $" + wages);
+    }
 }
